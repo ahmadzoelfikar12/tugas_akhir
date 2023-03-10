3bbff3a17f40ba842bbf8471c492bc3f777f6ee7
imas <imasnurdianto2002@gmail.com>
2023-03-11 04:21:58 +0700

    update login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,1 +1,74 @@
-Mamat Asu
+<html lang="en" data-bs-theme="dark">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN | LIDITUNE</title>
+    <link rel="stylesheet" href="assets/css/main/custom.css">
+    <link rel="stylesheet" href="assets/css/main/app.css">
+    <link rel="stylesheet" href="assets/css/main/app-dark.css">
+    <link rel="stylesheet" href="assets/css/pages/auth.css">
+    <link rel="shortcut icon" href="assets/images/logo/favicon.svg" type="image/x-icon">
+    <link rel="shortcut icon" href="assets/images/logo/favicon.png" type="image/png">
+    <link rel="icon" href="{{ asset('assets/logo1.png') }}" type="image/x-icon">
+</head>
+
+<body>
+    <script src="assets/js/initTheme.js"></script>
+    <div id="auth">
+        <div class="row">
+            <div class="col-lg-5 col-12">
+                <div id="auth-left">
+                    <div class="auth-logo">
+                        <a href="index.html"><img src="{{ asset('assets/logo1.png') }}" alt="Logo"style="height: 170px;"></a>
+                    </div>
+                    {{-- <h1 class="auth-title">Log in.</h1>
+                    <p class="auth-subtitle mb-5">
+                        Log in with your data that you entered during registration.
+                    </p> --}}
+
+                    <form action="index.html">
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="text" class="form-control form-control-xl btn-user" id="username" placeholder="Username">
+                            <div class="form-control-icon">
+                                <i class="bi bi-person"></i>
+                            </div>
+                        </div>
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="password" class="form-control form-control-xl" placeholder="Password">
+                            <div class="form-control-icon">
+                                <i class="bi bi-shield-lock"></i>
+                            </div>
+                        </div>
+                        {{-- <div class="form-check form-check-lg d-flex align-items-end">
+                            <input class="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault">
+                            <label class="form-check-label text-gray-600" for="flexCheckDefault">
+                                Keep me logged in
+                            </label>
+                        </div> --}}
+                        {{-- <button type="login" class="btn btn-login">Login</button> --}}
+                        <button class="btn-login btn-lg shadow-lg mt-5">
+                            LOGIN
+                        </button>
+                    </form>
+                    {{-- <div class="text-center mt-5 text-lg fs-4">
+                        <p class="text-gray-600">
+                            Don't have an account?
+                            <a href="auth-register.html" class="font-bold">Sign up</a>.
+                        </p>
+                        <p>
+                            <a class="font-bold" href="auth-forgot-password.html">Forgot password?</a>.
+                        </p>
+                    </div> --}}
+                </div>
+            </div>
+            <div class="col-lg-7 d-none d-lg-block">
+                <div id="auth-right"><a href="index.html"><img src="assets/18.png" alt="Logo"style="height: 600px; padding: 0% 0% 0% 0%;"></a></div>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
